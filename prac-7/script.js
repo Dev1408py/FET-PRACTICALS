e93abec81bf678470ddc7969d99fbc2896ef1c00
Dev1408py <95069438+Dev1408py@users.noreply.github.com>
2023-02-03 07:54:04 +0530

    Update script.js

--- prac-7/script.js
+++ prac-7/script.js
@@ -1,1 +1,26 @@
+import { calculate_rating } from './calculate_rating.js';
+import { process_payment } from './process_payment.js';
+import { validate_info } from './validate_info.js';
 
+function create_Account() {
+  let name = prompt("Enter the user name.");
+  let pass = prompt("Enter the password.");
+  if (validate_info(name, pass)) {
+    let card_number = prompt("Enter the card number.");
+    let amount = prompt("Enter the amount.");
+    let hold = process_payment(card_number, amount);
+    if (hold) {
+      alert(hold);
+      let n = prompt("Enter rating (1-->5)")
+      console.log(calculate_rating(n));
+    }
+    else{
+      alert("Payment unsuccesful");
+    }
+  }
+  else {
+    alert("Invalid user.");
+  }
+}
+
+create_Account();
